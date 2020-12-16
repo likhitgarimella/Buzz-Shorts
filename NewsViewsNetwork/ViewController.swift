2dfbbb704a81298bb4ee44bd74a5666aafd0c8e3
Likhit Garimella <likhitgmella@gmail.com>
2020-12-16 19:58:35 +0530

    scene delegate + VC setup

--- NewsViewsNetwork/ViewController.swift
+++ NewsViewsNetwork/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
-
-
-}
-
+    
+}   // #20
