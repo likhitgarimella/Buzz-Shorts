57b89e76d1d8290eb6c6dde44ee1f8c64d7a3b19
Likhit Garimella <likhitgmella@gmail.com>
2020-12-29 18:00:25 +0530

    menu ctrlr class delg fns

--- NewsViewsNetwork/ViewController.swift
+++ NewsViewsNetwork/ViewController.swift
@@ -44,16 +44,24 @@ class MenuController: UITableViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return menuItems.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        /// Relay to delegate about menu item selection
         
     }
     
-}   // #60
+}   // #68
