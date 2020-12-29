b8c7335dd3aa75f25c912c8ab736cdd79befddff
Likhit Garimella <likhitgmella@gmail.com>
2020-12-29 17:56:03 +0530

    menu controller class

--- NewsViewsNetwork/ViewController.swift
+++ NewsViewsNetwork/ViewController.swift
@@ -33,4 +33,27 @@ class ViewController: UIViewController {
         
     }
     
-}   // #37
+}
+
+class MenuController: UITableViewController {
+    
+    private let menuItems: [String]
+    
+    init(with menuItems: [String]) {
+        self.menuItems = menuItems
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+}   // #60
