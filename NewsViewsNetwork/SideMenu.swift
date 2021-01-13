c6f8cc965bbfa35318383748c44ec375ee7256ae
Likhit Garimella <likhitgmella@gmail.com>
2021-01-13 12:27:42 +0530

    keep side menu code in new file

---
+++ NewsViewsNetwork/SideMenu.swift
@@ -0,0 +1,61 @@
+//
+//  SideMenu.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 13/01/21.
+//
+
+import Foundation
+import UIKit
+
+protocol MenuControllerDelegate {
+    
+    func didSelectMenuItem(named: String)
+    
+}
+
+class MenuController: UITableViewController {
+    
+    public var delegate: MenuControllerDelegate?
+    
+    private let menuItems: [String]
+    
+    init(with menuItems: [String]) {
+        self.menuItems = menuItems
+        super.init(nibName: nil, bundle: nil)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.backgroundColor = .white
+        view.backgroundColor = .white
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        cell.textLabel?.textColor = .darkGray
+        cell.backgroundColor = .white
+        cell.contentView.backgroundColor = .white
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        /// Relay to delegate about menu item selection
+        let selectedItem = menuItems[indexPath.row]
+        delegate?.didSelectMenuItem(named: selectedItem)
+    }
+    
+}   // #62
