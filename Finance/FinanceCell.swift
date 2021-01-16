529aa15b1889d1d7d189a492865adedd3afcf873
Likhit Garimella <likhitgmella@gmail.com>
2021-01-17 01:58:00 +0530

    create finance cell

---
+++ Finance/FinanceCell.swift
@@ -0,0 +1,19 @@
+//
+//  FinanceCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 17/01/21.
+//
+
+import UIKit
+
+class FinanceCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
