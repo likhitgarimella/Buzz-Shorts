ff6b715968534e83644735cb22d76f9b6306056c
Likhit Garimella <likhitgmella@gmail.com>
2021-01-17 01:54:31 +0530

    create world cell

---
+++ World/WorldCell.swift
@@ -0,0 +1,19 @@
+//
+//  WorldCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 17/01/21.
+//
+
+import UIKit
+
+class WorldCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
