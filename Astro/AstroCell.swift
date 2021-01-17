cb33d1ec0164e3419a26c5e68fa3b2aa789871de
Likhit Garimella <likhitgmella@gmail.com>
2021-01-17 15:03:24 +0530

    create astro cell

---
+++ Astro/AstroCell.swift
@@ -0,0 +1,19 @@
+//
+//  AstroCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 17/01/21.
+//
+
+import UIKit
+
+class AstroCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
