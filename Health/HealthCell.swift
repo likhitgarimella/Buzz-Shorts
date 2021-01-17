bee7b57454fe483ad28a7472cb4d26f809d66ee4
Likhit Garimella <likhitgmella@gmail.com>
2021-01-17 14:48:31 +0530

    create health cell

---
+++ Health/HealthCell.swift
@@ -0,0 +1,19 @@
+//
+//  HealthCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 17/01/21.
+//
+
+import UIKit
+
+class HealthCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
