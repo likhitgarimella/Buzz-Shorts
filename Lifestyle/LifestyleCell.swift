a9a901d7f773b8ecc374e77c4f76d6fbc10680c7
Likhit Garimella <likhitgmella@gmail.com>
2021-01-17 14:49:44 +0530

    create lifestyle cell

---
+++ Lifestyle/LifestyleCell.swift
@@ -0,0 +1,19 @@
+//
+//  LifestyleCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 17/01/21.
+//
+
+import UIKit
+
+class LifestyleCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
