50226bc8cdce37bd98522bb668355bf8ca6ca425
Likhit Garimella <likhitgmella@gmail.com>
2021-01-17 14:35:01 +0530

    create auto cell

---
+++ Auto/AutoCell.swift
@@ -0,0 +1,19 @@
+//
+//  AutoCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 17/01/21.
+//
+
+import UIKit
+
+class AutoCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
