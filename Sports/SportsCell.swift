4705ec6567e1f93067e9291371e14eed6e069c70
Likhit Garimella <likhitgmella@gmail.com>
2021-01-17 14:37:48 +0530

    create sports cell

---
+++ Sports/SportsCell.swift
@@ -0,0 +1,19 @@
+//
+//  SportsCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 17/01/21.
+//
+
+import UIKit
+
+class SportsCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
