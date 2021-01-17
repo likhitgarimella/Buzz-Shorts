a9f210db0c1ae7fd6dc785fa563f17241824b013
Likhit Garimella <likhitgmella@gmail.com>
2021-01-17 14:41:16 +0530

    create entertainment cell

---
+++ Entertainment/EntertainmentCell.swift
@@ -0,0 +1,19 @@
+//
+//  EntertainmentCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 17/01/21.
+//
+
+import UIKit
+
+class EntertainmentCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
