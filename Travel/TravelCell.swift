f16511f012c88446f750f05316918f551ce15d6a
Likhit Garimella <likhitgmella@gmail.com>
2021-01-17 15:01:37 +0530

    create travel cell

---
+++ Travel/TravelCell.swift
@@ -0,0 +1,19 @@
+//
+//  TravelCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 17/01/21.
+//
+
+import UIKit
+
+class TravelCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
