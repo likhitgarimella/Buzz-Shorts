14fa52cf8c1d085187051945dfa1f9e3f8c6b377
Likhit Garimella <likhitgmella@gmail.com>
2021-01-17 14:33:24 +0530

    create tech cell

---
+++ Tech/TechCell.swift
@@ -0,0 +1,19 @@
+//
+//  TechCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 17/01/21.
+//
+
+import UIKit
+
+class TechCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
