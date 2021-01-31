5b7917357c42a94ec72925ddd1177e11391bfee2
Likhit Garimella <likhitgmella@gmail.com>
2021-01-31 23:50:55 +0530

    created model & post api files in india group

---
+++ India/IndiaModel.swift
@@ -0,0 +1,14 @@
+//
+//  IndiaModel.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 31/01/21.
+//
+
+import Foundation
+
+class IndiaModel {
+    
+    
+    
+}   // #15
---
+++ India/IndiaPostApi.swift
@@ -0,0 +1,17 @@
+//
+//  IndiaPostApi.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 31/01/21.
+//
+
+import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
+
+class IndiaPostApi {
+    
+    
+    
+}   // #18
