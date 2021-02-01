fe70cac8edcdedb7f714d994f7d85107f1bd1296
Likhit Garimella <likhitgmella@gmail.com>
2021-02-02 05:09:58 +0530

    model class & post api files created for travel

---
+++ Travel/TravelModel.swift
@@ -0,0 +1,14 @@
+//
+//  TravelModel.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class TravelModel {
+    
+    
+    
+}   // #15
---
+++ Travel/TravelPostApi.swift
@@ -0,0 +1,14 @@
+//
+//  TravelPostApi.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class TravelPostApi {
+    
+    
+    
+}   // #15
