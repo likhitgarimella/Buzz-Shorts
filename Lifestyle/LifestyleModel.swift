796a50071eb786c18254d359a2a7b056f98fda60
Likhit Garimella <likhitgmella@gmail.com>
2021-02-02 05:07:48 +0530

    model class & post api files created for lifestyle

---
+++ Lifestyle/LifestyleModel.swift
@@ -0,0 +1,14 @@
+//
+//  LifestyleModel.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class LifestyleModel {
+    
+    
+    
+}   // #15
---
+++ Lifestyle/LifestylePostApi.swift
@@ -0,0 +1,14 @@
+//
+//  LifestylePostApi.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class LifestylePostApi {
+    
+    
+    
+}   // #15
