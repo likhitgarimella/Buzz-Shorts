eebf1730265532875d12ecc1f63b5d621e313a3c
Likhit Garimella <likhitgmella@gmail.com>
2021-02-02 03:51:25 +0530

    ~~india~~ sb vc outlets connected; vc code #1

--- India/IndiaViewController.swift
+++ India/IndiaViewController.swift
@@ -6,8 +6,38 @@
 //
 
 import UIKit
+// import Firebase
 
-class IndiaViewController: UIViewController {
+class IndiaViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate {
+    
+    // Outlets
+    @IBOutlet var indiaCollectionView: UICollectionView!
+    
+    @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
+    
+    // reference to store IndiaModel class info
+    var indiaPosts = [IndiaModel]()
+    
+    // copy of reference
+    var realIndiaPosts = [IndiaModel]()
+    
+    // load home posts
+    func loadPosts() {
+        
+        /// start when loadPosts func starts
+        activityIndicatorView.startAnimating()
+        
+        Api.IndiaPost.observePosts { (post) in
+            self.indiaPosts.append(post)
+            print(self.indiaPosts)
+            /// stop before view reloads data
+            self.activityIndicatorView.stopAnimating()
+            self.activityIndicatorView.hidesWhenStopped = true
+            self.realIndiaPosts = self.indiaPosts
+            self.indiaCollectionView.reloadData()
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +46,4 @@ class IndiaViewController: UIViewController {
         
     }
     
-}   // #20
+}   // #50
