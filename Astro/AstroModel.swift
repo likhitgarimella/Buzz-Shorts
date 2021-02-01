267d9e33f3736373f61a9844a5aeb59babd31b04
Likhit Garimella <likhitgmella@gmail.com>
2021-02-02 05:11:30 +0530

    model class & post api files created for astro

---
+++ Astro/AstroModel.swift
@@ -0,0 +1,14 @@
+//
+//  AstroModel.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class AstroModel {
+    
+    
+    
+}   // #15
---
+++ Astro/AstroPostApi.swift
@@ -0,0 +1,14 @@
+//
+//  AstroPostApi.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class AstroPostApi {
+    
+    
+    
+}   // #15
