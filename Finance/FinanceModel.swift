f1ab133e0cf5a284e124b0482f23caf7126f8dda
Likhit Garimella <likhitgmella@gmail.com>
2021-02-02 04:33:54 +0530

    model class & post api files created for finance

---
+++ Finance/FinanceModel.swift
@@ -0,0 +1,14 @@
+//
+//  FinanceModel.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class FinanceModel {
+    
+    
+    
+}   // #15
---
+++ Finance/FinancePostApi.swift
@@ -0,0 +1,14 @@
+//
+//  FinancePostApi.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class FinancePostApi {
+    
+    
+    
+}   // #15
