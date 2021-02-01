63901a9a6b60579ee76a6c07881e83ccc6839e13
Likhit Garimella <likhitgmella@gmail.com>
2021-02-02 05:03:43 +0530

    model class & post api files created for entertainment

---
+++ Entertainment/EntertainmentModel.swift
@@ -0,0 +1,14 @@
+//
+//  EntertainmentModel.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class EntertainmentModel {
+    
+    
+    
+}   // #15
---
+++ Entertainment/EntertainmentPostApi.swift
@@ -0,0 +1,14 @@
+//
+//  EntertainmentPostApi.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class EntertainmentPostApi {
+    
+    
+    
+}   // #15
