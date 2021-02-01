c66d7890cc990360f104f48d1e9061b4f69aff47
Likhit Garimella <likhitgmella@gmail.com>
2021-02-02 04:31:52 +0530

    model class & post api files created for world

---
+++ World/WorldModel.swift
@@ -0,0 +1,14 @@
+//
+//  WorldModel.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class WorldModel {
+    
+    
+    
+}   // #15
---
+++ World/WorldPostApi.swift
@@ -0,0 +1,14 @@
+//
+//  WorldPostApi.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class WorldPostApi {
+    
+    
+    
+}   // #15
