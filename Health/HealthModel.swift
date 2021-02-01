19091e97ffff8437ddbf653125066047c898703e
Likhit Garimella <likhitgmella@gmail.com>
2021-02-02 05:05:01 +0530

    model class & post api files created for health

---
+++ Health/HealthModel.swift
@@ -0,0 +1,14 @@
+//
+//  HealthModel.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class HealthModel {
+    
+    
+    
+}   // #15
---
+++ Health/HealthPostApi.swift
@@ -0,0 +1,14 @@
+//
+//  HealthPostApi.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 02/02/21.
+//
+
+import Foundation
+
+class HealthPostApi {
+    
+    
+    
+}   // #15
