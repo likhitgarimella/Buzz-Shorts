c1ecb1f8e6ea8430b416fa446e413b430f62ce47
Likhit Garimella <likhitgmella@gmail.com>
2021-02-06 03:08:40 +0530

    vc files added for sections 22,23,24

---
+++ Gaming/GamingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GamingViewController.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 06/02/21.
+//
+
+import UIKit
+
+class GamingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #20
---
+++ Hot from the Oven/HotOvenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HotOvenViewController.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 06/02/21.
+//
+
+import UIKit
+
+class HotOvenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #20
