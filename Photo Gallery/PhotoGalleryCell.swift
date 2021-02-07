586764e2d104a4e0cf7357cb0b8397de1c0214c2
Likhit Garimella <likhitgmella@gmail.com>
2021-02-07 20:02:26 +0530

    xib cell files added for sections 25,27

---
+++ Photo Gallery/PhotoGalleryCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoGalleryCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 07/02/21.
+//
+
+import UIKit
+
+class PhotoGalleryCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
