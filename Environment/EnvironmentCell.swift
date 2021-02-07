a60b4b5991872b05517bbfb07e8838f7ff56bdad
Likhit Garimella <likhitgmella@gmail.com>
2021-02-07 19:29:46 +0530

    xib cell files added for sections 16,17,18

---
+++ Environment/EnvironmentCell.swift
@@ -0,0 +1,19 @@
+//
+//  EnvironmentCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 07/02/21.
+//
+
+import UIKit
+
+class EnvironmentCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
---
+++ Offbeat News/OffbeatNewsCell.swift
@@ -0,0 +1,19 @@
+//
+//  OffbeatNewsCell.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 07/02/21.
+//
+
+import UIKit
+
+class OffbeatNewsCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #20
