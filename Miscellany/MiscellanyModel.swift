3df032ffd11ee69bd062b9c65f4d95bc030cb3b0
Likhit Garimella <likhitgmella@gmail.com>
2021-02-07 23:42:11 +0530

    model & post api files added for section-27

---
+++ Miscellany/MiscellanyModel.swift
@@ -0,0 +1,14 @@
+//
+//  MiscellanyModel.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 07/02/21.
+//
+
+import Foundation
+
+class MiscellanyModel {
+    
+    
+    
+}   // #15
---
+++ Miscellany/MiscellanyPostApi.swift
@@ -0,0 +1,14 @@
+//
+//  MiscellanyPostApi.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 07/02/21.
+//
+
+import Foundation
+
+class MiscellanyPostApi {
+    
+    
+    
+}   // #15
