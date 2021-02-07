86a7efe69a775ab420ad58f7275839797de5433a
Likhit Garimella <likhitgmella@gmail.com>
2021-02-07 23:41:03 +0530

    model & post api files added for section-25

---
+++ Photo Gallery/PhotoModel.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoModel.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 07/02/21.
+//
+
+import Foundation
+
+class PhotoModel {
+    
+    
+    
+}   // #15
---
+++ Photo Gallery/PhotoPostApi.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoPostApi.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 07/02/21.
+//
+
+import Foundation
+
+class PhotoPostApi {
+    
+    
+    
+}   // #15
