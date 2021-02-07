2fa82f7dc1000aea0d6f0c6c5ea76d976b59ac9d
Likhit Garimella <likhitgmella@gmail.com>
2021-02-07 19:02:32 +0530

    videos vc done with youtube embeded

--- Miscellany/MiscellanyViewController.swift
+++ Miscellany/MiscellanyViewController.swift
@@ -12,7 +12,7 @@ class MiscellanyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.white
+        view.backgroundColor = UIColor.green
         
     }
     
