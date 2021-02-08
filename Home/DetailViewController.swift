3b604496a872e2f554924e084a83b41c17d30170
Likhit Garimella <likhitgmella@gmail.com>
2021-02-08 06:50:20 +0530

    pass coll view cell data #1

---
+++ Home/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  NewsViewsNetwork
+//
+//  Created by Likhit Garimella on 08/02/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #20
