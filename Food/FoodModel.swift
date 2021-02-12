6a0c51d79fc14d86bc8e682d19c8cf69b85a5a2e
Likhit Garimella <likhitgmella@gmail.com>
2021-02-12 21:38:13 +0530

    FoodModel done

--- Food/FoodModel.swift
+++ Food/FoodModel.swift
@@ -9,6 +9,31 @@ import Foundation
 
 class FoodModel {
     
+    /// Remodel Post class, bcuz it currently doesn't have a post id property
+    var id: String?
     
+    var photoUrl: String?
     
-}   // #15
+    var leftTag: String?
+    var heading: String?
+    var readTime: String?
+    
+}
+
+extension FoodModel {
+    
+    // Photo
+    static func transformPostPhoto(dict: [String: Any], key: String) -> FoodModel {
+        
+        let post = FoodModel()
+        /// Remodel Post class, bcuz it currently doesn't have a post id property
+        post.id = key
+        post.photoUrl = dict["1) Photo Url"] as? String
+        post.leftTag = dict["2) Left Tag"] as? String
+        post.heading = dict["3) Heading"] as? String
+        post.readTime = dict["4) Read Time"] as? String
+        return post
+        
+    }
+    
+}   // #40
