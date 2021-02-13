44e624635a0058065e222d74ade90b6c0f202952
Likhit Garimella <likhitgmella@gmail.com>
2021-02-13 11:58:06 +0530

    WorldPostApi done

--- World/WorldPostApi.swift
+++ World/WorldPostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class WorldPostApi {
     
+    var REF_POSTS = Database.database().reference().child("World-Posts").child("Details")
     
+    func observePosts(completion: @escaping (WorldModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = WorldModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (WorldModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = WorldModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
