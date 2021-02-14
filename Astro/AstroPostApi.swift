47ebf1a5a93bb4938379247bc3b06eb13772c9ce
Likhit Garimella <likhitgmella@gmail.com>
2021-02-15 04:00:26 +0530

    AstroPostApi done

--- Astro/AstroPostApi.swift
+++ Astro/AstroPostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class AstroPostApi {
     
+    var REF_POSTS = Database.database().reference().child("Astro-Posts").child("Details")
     
+    func observePosts(completion: @escaping (AstroModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = AstroModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (AstroModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = AstroModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
