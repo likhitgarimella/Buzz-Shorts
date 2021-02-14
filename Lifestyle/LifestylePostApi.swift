21e59c2b78fb81eb129cec857bf607c2e637f295
Likhit Garimella <likhitgmella@gmail.com>
2021-02-14 23:24:41 +0530

    LifestylePostApi done

--- Lifestyle/LifestylePostApi.swift
+++ Lifestyle/LifestylePostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class LifestylePostApi {
     
+    var REF_POSTS = Database.database().reference().child("Lifestyle-Posts").child("Details")
     
+    func observePosts(completion: @escaping (LifestyleModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = LifestyleModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (LifestyleModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = LifestyleModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
