bc0a15d281b18eb94dddb4c51d2dce276bbde216
Likhit Garimella <likhitgmella@gmail.com>
2021-02-14 09:22:22 +0530

    SportsPostApi done

--- Sports/SportsPostApi.swift
+++ Sports/SportsPostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class SportsPostApi {
     
+    var REF_POSTS = Database.database().reference().child("Sports-Posts").child("Details")
     
+    func observePosts(completion: @escaping (SportsModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = SportsModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (SportsModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = SportsModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
