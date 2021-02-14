7d2fdc77b622beb3bd4a3cd143214c3faa829948
Likhit Garimella <likhitgmella@gmail.com>
2021-02-14 08:24:10 +0530

    TechPostApi done

--- Tech/TechPostApi.swift
+++ Tech/TechPostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class TechPostApi {
     
+    var REF_POSTS = Database.database().reference().child("Tech-Posts").child("Details")
     
+    func observePosts(completion: @escaping (TechModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = TechModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (TechModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = TechModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
