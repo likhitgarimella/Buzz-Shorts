e3b900a63ded5488fa740b66e9d00beb542b1b7e
Likhit Garimella <likhitgmella@gmail.com>
2021-02-14 23:07:05 +0530

    HealthPostApi done

--- Health/HealthPostApi.swift
+++ Health/HealthPostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class HealthPostApi {
     
+    var REF_POSTS = Database.database().reference().child("Health-Posts").child("Details")
     
+    func observePosts(completion: @escaping (HealthModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = HealthModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (HealthModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = HealthModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
