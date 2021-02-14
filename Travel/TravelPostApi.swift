660dedd2939d164df943905982d59700a48c0a3a
Likhit Garimella <likhitgmella@gmail.com>
2021-02-15 03:58:07 +0530

    TravelPostApi done

--- Travel/TravelPostApi.swift
+++ Travel/TravelPostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class TravelPostApi {
     
+    var REF_POSTS = Database.database().reference().child("Travel-Posts").child("Details")
     
+    func observePosts(completion: @escaping (TravelModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = TravelModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (TravelModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = TravelModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
