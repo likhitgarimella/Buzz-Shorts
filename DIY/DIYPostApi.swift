75d933f5832f2de14e2438e6aa7fcc732d326056
Likhit Garimella <likhitgmella@gmail.com>
2021-02-15 23:05:44 +0530

    DIYPostApi done

--- DIY/DIYPostApi.swift
+++ DIY/DIYPostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class DIYPostApi {
     
+    var REF_POSTS = Database.database().reference().child("DIY-Posts").child("Details")
     
+    func observePosts(completion: @escaping (DIYModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = DIYModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (DIYModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = DIYModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
