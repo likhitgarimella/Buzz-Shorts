0fb9b2d37d278adba1a02f1f9e3c1587f3549612
Likhit Garimella <likhitgmella@gmail.com>
2021-02-15 11:37:16 +0530

    TopTenPostApi done

--- Top Ten/TopTenPostApi.swift
+++ Top Ten/TopTenPostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class TopTenPostApi {
     
+    var REF_POSTS = Database.database().reference().child("TopTen-Posts").child("Details")
     
+    func observePosts(completion: @escaping (TopTenModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = TopTenModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (TopTenModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = TopTenModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
