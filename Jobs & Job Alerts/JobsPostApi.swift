46cb6059480c4e5ad956af603588ac46702900c2
Likhit Garimella <likhitgmella@gmail.com>
2021-02-15 23:18:53 +0530

    JobsPostApi done

--- Jobs & Job Alerts/JobsPostApi.swift
+++ Jobs & Job Alerts/JobsPostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class JobsPostApi {
     
+    var REF_POSTS = Database.database().reference().child("Jobs-Posts").child("Details")
     
+    func observePosts(completion: @escaping (JobsModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = JobsModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (JobsModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = JobsModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
