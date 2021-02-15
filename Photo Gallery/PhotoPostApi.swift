aaac3656425442d6973d291c4b3745bd85e0ca9f
Likhit Garimella <likhitgmella@gmail.com>
2021-02-15 23:21:44 +0530

    PhotoPostApi done

--- Photo Gallery/PhotoPostApi.swift
+++ Photo Gallery/PhotoPostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class PhotoPostApi {
     
+    var REF_POSTS = Database.database().reference().child("Photo-Posts").child("Details")
     
+    func observePosts(completion: @escaping (PhotoModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = PhotoModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (PhotoModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = PhotoModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
