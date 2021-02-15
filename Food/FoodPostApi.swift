f7e8becb3fe6ae1639dc7d72cbb667c45d6b5ee5
Likhit Garimella <likhitgmella@gmail.com>
2021-02-15 11:04:14 +0530

    FoodPostApi done

--- Food/FoodPostApi.swift
+++ Food/FoodPostApi.swift
@@ -6,9 +6,39 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+/// Write your own Api, to conveniently observe database data...
 
 class FoodPostApi {
     
+    var REF_POSTS = Database.database().reference().child("Food-Posts").child("Details")
     
+    func observePosts(completion: @escaping (FoodModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = FoodModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #15
+    func observePost(withId id: String, completion: @escaping (FoodModel) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = FoodModel.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #45
