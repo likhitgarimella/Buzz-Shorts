1f24517b513dc97c5d52ef68f9d3219674acb105
Likhit Garimella <likhitgmella@gmail.com>
2021-02-16 21:01:59 +0530

    xib code done for WorldCell

--- World/WorldCell.swift
+++ World/WorldCell.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+// import Firebase
+import SDWebImage
+
+/// If a View needs data, it should ask controllers...
 
 class WorldCell: UICollectionViewCell {
+    
+    // Outlets
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var widthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var worldNewsPic: UIImageView!
+    @IBOutlet weak var leftTagString: UILabel!
+    @IBOutlet weak var headingString: UILabel!
+    @IBOutlet weak var readTimeString: UILabel!
+    
+    // linking feed VC & feed cell
+    var worldFeedVC: WorldViewController?
+    
+    var worldPost: WorldModel? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        
+        if let photoUrlString = worldPost?.photoUrl {
+            let photoUrl = URL(string: photoUrlString)
+            worldNewsPic.sd_setImage(with: photoUrl)
+        }
+        
+        leftTagString.text = worldPost?.leftTag
+        headingString.text = worldPost?.heading
+        readTimeString.text = worldPost?.readTime
+        
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        // initial text
+        leftTagString.text = ""
+        headingString.text = ""
+        readTimeString.text = ""
         
+        // number of lines
+        leftTagString.numberOfLines = 0
+        headingString.numberOfLines = 0
+        readTimeString.numberOfLines = 0
+        
+        // constraint
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        let screenWidth = UIScreen.main.bounds.size.width
+        widthConstraint.constant = screenWidth - (2 * 12)
         
     }
     
-}   // #20
+}   // #68
