b5fcb45e4282df9a1c0a1843696db9c0f4012eff
Likhit Garimella <likhitgmella@gmail.com>
2021-02-16 23:43:08 +0530

    xib code done for AutoCell

--- Auto/AutoCell.swift
+++ Auto/AutoCell.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+// import Firebase
+import SDWebImage
+
+/// If a View needs data, it should ask controllers...
 
 class AutoCell: UICollectionViewCell {
+    
+    // Outlets
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var widthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var autoNewsPic: UIImageView!
+    @IBOutlet weak var leftTagString: UILabel!
+    @IBOutlet weak var headingString: UILabel!
+    @IBOutlet weak var readTimeString: UILabel!
+    
+    // linking feed VC & feed cell
+    var autoFeedVC: AutoViewController?
+    
+    var autoPost: AutoModel? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        
+        if let photoUrlString = autoPost?.photoUrl {
+            let photoUrl = URL(string: photoUrlString)
+            autoNewsPic.sd_setImage(with: photoUrl)
+        }
+        
+        leftTagString.text = autoPost?.leftTag
+        headingString.text = autoPost?.heading
+        readTimeString.text = autoPost?.readTime
+        
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        // initial text
+        leftTagString.text = ""
+        headingString.text = ""
+        readTimeString.text = ""
         
+        // number of lines
+        leftTagString.numberOfLines = 0
+        headingString.numberOfLines = 0
+        readTimeString.numberOfLines = 0
+        
+        // constraint
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        let screenWidth = UIScreen.main.bounds.size.width
+        widthConstraint.constant = screenWidth - (2 * 12)
         
     }
     
-}   // #20
+}   // #68
