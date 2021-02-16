d165b195130f1d75ffdcfeb048dc0a9153e979c0
Likhit Garimella <likhitgmella@gmail.com>
2021-02-16 23:02:32 +0530

    xib code done for FinanceCell

--- Finance/FinanceCell.swift
+++ Finance/FinanceCell.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+// import Firebase
+import SDWebImage
+
+/// If a View needs data, it should ask controllers...
 
 class FinanceCell: UICollectionViewCell {
+    
+    // Outlets
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var widthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var financeNewsPic: UIImageView!
+    @IBOutlet weak var leftTagString: UILabel!
+    @IBOutlet weak var headingString: UILabel!
+    @IBOutlet weak var readTimeString: UILabel!
+    
+    // linking feed VC & feed cell
+    var financeFeedVC: FinanceViewController?
+    
+    var financePost: FinanceModel? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        
+        if let photoUrlString = financePost?.photoUrl {
+            let photoUrl = URL(string: photoUrlString)
+            financeNewsPic.sd_setImage(with: photoUrl)
+        }
+        
+        leftTagString.text = financePost?.leftTag
+        headingString.text = financePost?.heading
+        readTimeString.text = financePost?.readTime
+        
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        // initial text
+        leftTagString.text = ""
+        headingString.text = ""
+        readTimeString.text = ""
         
+        // number of lines
+        leftTagString.numberOfLines = 0
+        headingString.numberOfLines = 0
+        readTimeString.numberOfLines = 0
+        
+        // constraint
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        let screenWidth = UIScreen.main.bounds.size.width
+        widthConstraint.constant = screenWidth - (2 * 12)
         
     }
     
-}   // #20
+}   // #68
