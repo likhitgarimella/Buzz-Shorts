50f0a89348d65f80969c676867242065b3f43120
Likhit Garimella <likhitgmella@gmail.com>
2021-02-17 10:55:33 +0530

    xib code done for LifestyleCell

--- Lifestyle/LifestyleCell.swift
+++ Lifestyle/LifestyleCell.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+// import Firebase
+import SDWebImage
+
+/// If a View needs data, it should ask controllers...
 
 class LifestyleCell: UICollectionViewCell {
+    
+    // Outlets
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var widthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var lifestyleNewsPic: UIImageView!
+    @IBOutlet weak var leftTagString: UILabel!
+    @IBOutlet weak var headingString: UILabel!
+    @IBOutlet weak var readTimeString: UILabel!
+    
+    // linking feed VC & feed cell
+    var lifestyleFeedVC: LifestyleViewController?
+    
+    var lifestylePost: LifestyleModel? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        
+        if let photoUrlString = lifestylePost?.photoUrl {
+            let photoUrl = URL(string: photoUrlString)
+            lifestyleNewsPic.sd_setImage(with: photoUrl)
+        }
+        
+        leftTagString.text = lifestylePost?.leftTag
+        headingString.text = lifestylePost?.heading
+        readTimeString.text = lifestylePost?.readTime
+        
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        // initial text
+        leftTagString.text = ""
+        headingString.text = ""
+        readTimeString.text = ""
         
+        // number of lines
+        leftTagString.numberOfLines = 0
+        headingString.numberOfLines = 0
+        readTimeString.numberOfLines = 0
+        
+        // constraint
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        let screenWidth = UIScreen.main.bounds.size.width
+        widthConstraint.constant = screenWidth - (2 * 12)
         
     }
     
-}   // #20
+}   // #68
