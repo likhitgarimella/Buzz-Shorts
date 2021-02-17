d15b7ab191c2bf5f063474e79637d225fc751d02
Likhit Garimella <likhitgmella@gmail.com>
2021-02-17 11:51:59 +0530

    xib code done for TravelCell

--- Travel/TravelCell.swift
+++ Travel/TravelCell.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+// import Firebase
+import SDWebImage
+
+/// If a View needs data, it should ask controllers...
 
 class TravelCell: UICollectionViewCell {
+    
+    // Outlets
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var widthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var travelNewsPic: UIImageView!
+    @IBOutlet weak var leftTagString: UILabel!
+    @IBOutlet weak var headingString: UILabel!
+    @IBOutlet weak var readTimeString: UILabel!
+    
+    // linking feed VC & feed cell
+    var travelFeedVC: TravelViewController?
+    
+    var travelPost: TravelModel? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        
+        if let photoUrlString = travelPost?.photoUrl {
+            let photoUrl = URL(string: photoUrlString)
+            travelNewsPic.sd_setImage(with: photoUrl)
+        }
+        
+        leftTagString.text = travelPost?.leftTag
+        headingString.text = travelPost?.heading
+        readTimeString.text = travelPost?.readTime
+        
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        // initial text
+        leftTagString.text = ""
+        headingString.text = ""
+        readTimeString.text = ""
         
+        // number of lines
+        leftTagString.numberOfLines = 0
+        headingString.numberOfLines = 0
+        readTimeString.numberOfLines = 0
+        
+        // constraint
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        let screenWidth = UIScreen.main.bounds.size.width
+        widthConstraint.constant = screenWidth - (2 * 12)
         
     }
     
-}   // #20
+}   // #68
