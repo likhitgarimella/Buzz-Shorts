bfcfc0f185029417d32243326106faf9c7c22478
Likhit Garimella <likhitgmella@gmail.com>
2021-02-17 12:36:36 +0530

    xib code done for TopTenCell

--- Top Ten/TopTenCell.swift
+++ Top Ten/TopTenCell.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+// import Firebase
+import SDWebImage
+
+/// If a View needs data, it should ask controllers...
 
 class TopTenCell: UICollectionViewCell {
+    
+    // Outlets
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var widthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var toptenNewsPic: UIImageView!
+    @IBOutlet weak var leftTagString: UILabel!
+    @IBOutlet weak var headingString: UILabel!
+    @IBOutlet weak var readTimeString: UILabel!
+    
+    // linking feed VC & feed cell
+    var toptenFeedVC: TopTenViewController?
+    
+    var toptenPost: TopTenModel? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        
+        if let photoUrlString = toptenPost?.photoUrl {
+            let photoUrl = URL(string: photoUrlString)
+            toptenNewsPic.sd_setImage(with: photoUrl)
+        }
+        
+        leftTagString.text = toptenPost?.leftTag
+        headingString.text = toptenPost?.heading
+        readTimeString.text = toptenPost?.readTime
+        
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        // initial text
+        leftTagString.text = ""
+        headingString.text = ""
+        readTimeString.text = ""
         
+        // number of lines
+        leftTagString.numberOfLines = 0
+        headingString.numberOfLines = 0
+        readTimeString.numberOfLines = 0
+        
+        // constraint
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        let screenWidth = UIScreen.main.bounds.size.width
+        widthConstraint.constant = screenWidth - (2 * 12)
         
     }
     
-}   // #20
+}   // #68
